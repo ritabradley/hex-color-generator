e605fc48b828fe692b45b2ded93f5dd6929bdae7
Restyled.io <commits@restyled.io>
2022-10-16 23:30:38 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const container = document.querySelector('.container');
-let hexSpan = document.querySelector('.hex');
-const hexCode = document.querySelector('.hex-code');
-const genBtn = document.querySelector('.hex-generate');
+const container = document.querySelector(".container");
+let hexSpan = document.querySelector(".hex");
+const hexCode = document.querySelector(".hex-code");
+const genBtn = document.querySelector(".hex-generate");
 
 const generateRandomHexColor = () => {
   const hexChars = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
-    'f'
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
   ];
-  let hex = '#';
+  let hex = "#";
 
   while (hex.length < 7) {
     hex += hexChars[Math.floor(Math.random() * hexChars.length)];
@@ -20,4 +34,4 @@ const generateRandomHexColor = () => {
   hexCode.style.color = hex;
 };
 
-genBtn.addEventListener('click', generateRandomHexColor);
+genBtn.addEventListener("click", generateRandomHexColor);
